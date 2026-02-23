6a54298b8b1ecadc7df85dc180d880337f194862
new_pointer <new_chenyiliang@outlook.com>
2026-02-23 18:54:33 +0800

    回退更改

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ module.exports = (req, res) => {
     const width = textWidth;
     const height = fontSize;
     
-    const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}"><defs><linearGradient id="g" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#FE7398"/><stop offset="100%" stop-color="#6AD3F7"/></linearGradient></defs><text x="0" y="11" fill="url(#g)" font-size="${fontSize}" font-family="Arial,sans-serif" font-weight="bold">${age}</text></svg>`;
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}"><defs><linearGradient id="g" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#FE7398"/><stop offset="100%" stop-color="#6AD3F7"/></linearGradient></defs><text x="0" y="${fontSize * 0.85}" fill="url(#g)" font-size="${fontSize}" font-family="Arial,sans-serif" font-weight="bold">${age}</text></svg>`;
     
     res.status(200).send(svg);
 };
